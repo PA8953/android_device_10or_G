32a2f6f2390fe5f5133516dd117a6c08996d7081
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-16 20:34:05 +0800

    G: Import FCM from LA.UM.9.6.3.r1-06200-89xx.0
    https: //git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/core-utils/-/blob/LA.UM.9.6.3.r1-06200-89xx.0/vendor_framework_compatibility_matrix.xml
    Change-Id: Id0c713a5ff33758194efaf1bf4ba4cccfaf3411a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,6 +75,7 @@ TARGET_QCOM_NO_FM_FIRMWARE := true
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
